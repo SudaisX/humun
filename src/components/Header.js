e2901d8547108b1cebe4e1219227c4125967a6a6
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-12-23 22:55:06 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = ({ active }) => {
     return (
