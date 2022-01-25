f976eeb937f168042a57fcdcba45317ec8ffb33e
Sudais <44402465+sudaisx@users.noreply.github.com>
2022-01-25 15:59:20 +0500

    testing base route

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/humun'>
         <App />
     </BrowserRouter>,
     document.getElementById('root')
